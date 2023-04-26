008da7def5a264cb8b0e544ab930a4ee1ad5f6b2
LHD <sunupdong@gmail.com>
2023-04-26 23:44:09 +0800

    docs: FirstUniqueCharIndex

--- .vitepress/theme/typeChallenges/09286-medium-firstuniquecharindex.ts
+++ .vitepress/theme/typeChallenges/09286-medium-firstuniquecharindex.ts
@@ -5,9 +5,20 @@ type cases = [
   Expect<Equal<FirstUniqueCharIndex<'leetcode'>, 0>>,
   Expect<Equal<FirstUniqueCharIndex<'loveleetcode'>, 2>>,
   Expect<Equal<FirstUniqueCharIndex<'aabb'>, -1>>,
-  Expect<Equal<FirstUniqueCharIndex<''>, -1>>,
+  Expect<Equal<FirstUniqueCharIndex<''>, -1>>
 ]
 
-
 // ============= Your Code Here =============
-type FirstUniqueCharIndex<T extends string, i extends number = -1, U = T> = T extends `${infer H}${infer R}` ? '' : ''
+type FirstUniqueCharIndex<
+  T extends string,
+  _Acc extends string[] = []
+> = T extends ''
+  ? -1
+  : T extends `${infer H}${infer R}`
+  ? H extends _Acc[number]
+    ? FirstUniqueCharIndex<R, [..._Acc, H]>
+    : R extends `${string}${H}${string}`
+    ? FirstUniqueCharIndex<R, [..._Acc, H]>
+    : _Acc['length']
+  : never
+
