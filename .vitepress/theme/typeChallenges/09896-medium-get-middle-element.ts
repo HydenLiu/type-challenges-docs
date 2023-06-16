4883ce97f5f3d2896c72f3bf8da2115fcde0f61b
hyden <sunupdong@gmail.com>
2023-06-17 01:18:56 +0800

    fix: add GetMiddleElement

---
+++ .vitepress/theme/typeChallenges/09896-medium-get-middle-element.ts
@@ -0,0 +1,22 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<GetMiddleElement<[]>, []>>,
+  Expect<Equal<GetMiddleElement<[1, 2, 3, 4, 5]>, [3]>>,
+  Expect<Equal<GetMiddleElement<[1, 2, 3, 4, 5, 6]>, [3, 4]>>,
+  Expect<Equal<GetMiddleElement<[() => string]>, [() => string]>>,
+  Expect<Equal<GetMiddleElement<[() => number, '3', [3, 4], 5]>, ['3', [3, 4]]>>,
+  Expect<Equal<GetMiddleElement<[() => string, () => number]>, [() => string, () => number]>>,
+  Expect<Equal<GetMiddleElement<[never]>, [never]>>,
+]
+// @ts-expect-error
+type error = GetMiddleElement<1, 2, 3>
+
+
+// ============= Your Code Here =============
+type GetMiddleElement<T extends unknown[]> = T['length'] extends 0 | 1 | 2
+  ? T
+  : T extends [unknown, ...infer M, unknown]
+  ? GetMiddleElement<M>
+  : never
