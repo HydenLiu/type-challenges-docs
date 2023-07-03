04fcb40e273ee02ab885791b87cad17c8c9082ac
hyden <sunupdong@gmail.com>
2023-07-03 22:32:43 +0800

    feat: add Integer

---
+++ .vitepress/theme/typeChallenges/10969-medium-integer.ts
@@ -0,0 +1,23 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+import { ExpectFalse, NotEqual } from './test-utils'
+
+let x = 1
+let y = 1 as const
+
+type cases1 = [
+  Expect<Equal<Integer<1>, 1>>,
+  Expect<Equal<Integer<1.1>, never>>,
+  Expect<Equal<Integer<1.0>, 1>>,
+  Expect<Equal<Integer<1.000000000>, 1>>,
+  Expect<Equal<Integer<typeof x>, never>>,
+  Expect<Equal<Integer<typeof y>, 1>>,
+]
+
+
+// ============= Your Code Here =============
+type Integer<T extends number> = number extends T
+  ? never
+  : `${T}` extends `${number}.${number}`
+  ? never
+  : T
