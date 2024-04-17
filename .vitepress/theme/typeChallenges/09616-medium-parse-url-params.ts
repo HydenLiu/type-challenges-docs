4d41a81e6ad9711c7becaefddae13f64c5fc97da
Hyden Liu <sunupdong@gmail.com>
2024-04-17 16:13:54 +0800

    Create 09616-medium-parse-url-params.ts

---
+++ .vitepress/theme/typeChallenges/09616-medium-parse-url-params.ts
@@ -0,0 +1,18 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<ParseUrlParams<''>, never>>,
+  Expect<Equal<ParseUrlParams<':id'>, 'id'>>,
+  Expect<Equal<ParseUrlParams<'posts/:id'>, 'id'>>,
+  Expect<Equal<ParseUrlParams<'posts/:id/'>, 'id'>>,
+  Expect<Equal<ParseUrlParams<'posts/:id/:user'>, 'id' | 'user'>>,
+  Expect<Equal<ParseUrlParams<'posts/:id/:user/like'>, 'id' | 'user'>>
+]
+
+// ============= Your Code Here =============
+type ParseUrlParams<T> = T extends `${string}:${infer R}`
+  ? R extends `${infer A}/${infer B}`
+    ? A | ParseUrlParams<B>
+    : R
+  : never
